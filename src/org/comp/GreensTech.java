8986447c88c894b62438d0d1c977247220c3774b
KarthikeyanEzhumalai <karthikjks513@gmail.com>
2023-08-12 19:34:03 +0530

    Greensaddress Coding Done

---
+++ src/org/comp/GreensTech.java
@@ -0,0 +1,54 @@
+package org.comp;
+
+public class GreensTech {
+
+	private void address() {
+		System.out.println("GreensOmr");
+			}
+	private void address1() {
+		System.out.println("GreensAdayar");
+	}
+	private void address2() {
+		System.out.println("GreensTambaram");
+	}
+	private void address3() {
+		System.out.println("GreensVelacherry");
+	}
+	private void address4() {
+		System.out.println("GreensAnnaNagar");
+	}
+	
+	public static void main(String[] args) {
+		//ClassName objName = new ClassName();
+		GreensTech a = new GreensTech();
+		a.address();
+		a.address1();
+		a.address2();
+		a.address3();
+		a.address4();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
